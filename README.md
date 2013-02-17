c4a24d24ebfaef15f462e2f7c9044136db5ad87b
Attila Incze <attila.incze@gmail.com>
2013-02-16 23:47:25 -0800

    Gruntifying, adding functional test stub

--- README.md
+++ README.md
@@ -1,26 +1,17 @@
-dropdafile
-==========
+# dropdafile
 
 HTML5 Streaming file transfer server in node.js
-
 Live presentation: http://dropdafile.com
 
-## Technical details
-
-Some description here and code comments / tests coming soon..
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/gruntjs/grunt).
 
 ## Credits
-
 Design elements and icons used from:
 * http://dribbble.com/shots/146820-Navigation-Bar-Header
 * http://dribbble.com/shots/532992-Mini-Music-Player
 * http://www.wpzoom.com/wpzoom/new-freebie-wpzoom-developer-icon-set-154-free-icons/
 
 ## License
-
-(C) 2012 Attila Incze <attila.incze@gmail.com>
-
-http://atimb.me
-
-This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a copy of this license, visit
-http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+Copyright (c) 2013 Attila Incze
+Licensed under the MIT license.
---
+++ grunt.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: '<json:package.json>',
+        mochaTest: {
+            files: ['test/**/*.js']
+        },
+        mochaTestConfig: {
+            options: {
+                reporter: 'spec'
+            }
+        },
+        lint: {
+            files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+        },
+        watch: {
+            files: '<config:lint.files>',
+            tasks: 'default'
+        },
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                node: true
+            },
+            globals: {
+                exports: true
+            }
+        }
+    });
+
+    // Additional Tasks
+    grunt.registerTask('test', 'mochaTest');
+    grunt.registerTask('default', 'lint test');
+
+};
